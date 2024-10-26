10c372bcf73a9210ace8a6c172048b3e10dfc13e
tiagosousa <tiagosousa.tams@hotmail.com>
2024-10-26 19:25:57 +0200

    inicio ProjetoFORM com TypeScript e React

---
+++ 14_react_hook_form_com_ts/src/App.tsx
@@ -0,0 +1,13 @@
+import Form from './components/Form';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-bold text-3xl'>React HOOk Form com TypScript</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
